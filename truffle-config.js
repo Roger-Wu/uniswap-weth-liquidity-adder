284d96b10faf0eddd976910166b3fc50cc95ff8b
Roger-Wu <gsetcrw@gmail.com>
2019-08-16 19:17:23 +0800

    build to normal path

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  // contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
     develop: {
       port: 8545
